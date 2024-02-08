ecc33bef6633f1861e2336fa3892294674baed5b
rodrigueBONBON <rodrigue.bonbon@ecoles-epsi.net>
2024-02-08 13:54:13 +0100

    ajout du Jenkinsfile

--- src/App.java
+++ src/App.java
@@ -1,5 +1,4 @@
-
-
+package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Exécution de l'application de test unitaire...");
--- src/UnitTest.java
+++ src/UnitTest.java
@@ -1,4 +1,4 @@
-package
+package com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
